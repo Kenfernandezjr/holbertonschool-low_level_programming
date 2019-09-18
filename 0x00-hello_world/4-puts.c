da7eec862dbcd7b924c19afd0eaece04cb767ec5
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-18 22:11:10 +0000

    give me those word

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print string
+ * Return: void
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
