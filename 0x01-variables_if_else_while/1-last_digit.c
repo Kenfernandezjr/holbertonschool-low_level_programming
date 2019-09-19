7b59aa7a1b863212e2c83dc4502fa32cf6673629
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-19 23:14:49 +0000

    less than great than

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: - Void
+ *
+ * if - prints the last digit calculation and is applied to the equation
+ *
+ * else if - last digit is equal to 0
+ *
+ * else if - last digit will we be less than 6
+ */
+
+
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	lastDigit = n % 10;
+
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %i is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %i is %d and is 0\n", n, lastDigit);
+	}
+	else if (lastDigit < 6)
+	{
+		printf("Last digit of %i is %d and is less than 6 and not 0", n, lastDigit);
+	}
+	return (0);
+}
