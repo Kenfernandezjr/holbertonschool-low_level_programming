dfa46401c1d8fd1e33eacd9983f5e2061be82d3a
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-20 01:48:05 +0000

    print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - no return value
+ *
+ * Return: - void
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
