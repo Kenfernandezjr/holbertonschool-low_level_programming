c6db1b85d727740b51404a5b0bb902eb389b4b64
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-20 02:05:07 +0000

    numbers on numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - no return value
+ *
+ * Return: - void
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9' ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
