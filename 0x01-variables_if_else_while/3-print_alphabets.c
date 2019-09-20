1b62a58124eb2ef0dcbd83db1fb1de57e91abd71
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-20 00:36:05 +0000

    More alphabet soup

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - no return value
+ *
+ * Return: - void
+ */
+
+int main(void)
+{
+        char ch;
+
+        for (ch = 'a' ; ch <= 'z' ; ch++)
+        {
+                putchar(ch);
+        }
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+        putchar('\n');
+        return (0);
+}
