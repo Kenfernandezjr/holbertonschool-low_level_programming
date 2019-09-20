0dc4e33435dbe1410eb8fa6c245722617f58ec13
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-20 02:14:03 +0000

    reverse alphabet soup

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - no return value
+ *
+ * Return: - void
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch > 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
