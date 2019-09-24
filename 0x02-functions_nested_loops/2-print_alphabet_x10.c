2b720d4069eca992eb919296efa0b68c90b8a9f4
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-24 21:48:49 +0000

    nested loops to create 10 alphabet print out

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Entry access function
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
