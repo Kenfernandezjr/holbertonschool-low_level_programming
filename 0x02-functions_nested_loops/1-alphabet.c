39ed338633613ee5182a4af94fe4c6aaf71fc2c2
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-24 20:55:37 +0000

    print Alphabet with created functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Entry access function
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+
+	_putchar(ch);
+	}
+	_putchar('\n');
+}
