4af11b4eda712b965a7eef3a5a57a9e216cccda9
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-26 20:25:59 +0000

    print from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
-#
+
 
 /**
  * print_numbers - check the code for Holberton School students.
- * @c: int is numerials
- * _putchar: to print an chr
+ *
  */
+
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
