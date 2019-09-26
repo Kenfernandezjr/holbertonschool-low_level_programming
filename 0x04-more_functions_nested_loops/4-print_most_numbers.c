a580fe9be0bcb5399e3afdc1e0b06413dfad1fc4
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-26 21:42:47 +0000

    removing numbers using a for loop if statement

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+
+/**
+ * print_most_numbers - check the code for Holberton School students.
+ *
+ */
+
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' || c == '4')
+		{
+			continue;
+		}
+		_putchar(c);
+	}
+	_putchar('\n');
+}
