4b92befa9308e946911e94bea77584fa29e00c3a
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-26 19:37:33 +0000

    multiple int a and int b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a: the interger a for multipling
+ * @b: the interger b for multipling
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
