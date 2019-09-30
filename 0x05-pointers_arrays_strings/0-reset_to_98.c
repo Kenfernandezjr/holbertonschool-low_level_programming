3ab9fcfb8a1a09565823fa5aa65eacc07e8ab031
Kenfernandezjr <Kenfernandezjr@github.com>
2019-09-30 23:18:42 +0000

    Reset the variable using a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Entry point to begin coding
+ * @n: is the interger used for this exercise
+ * Return: - Void/Null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
