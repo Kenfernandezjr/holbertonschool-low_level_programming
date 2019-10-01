4af0c1a1810df98d84feb8460a6d0acdbb516ea2
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-01 22:12:25 +0000

    printing everyother character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,19 +4,12 @@
  * puts2 - check the code for Holberton School students.
  *
  * @str: it holds all place holders the string.
- *
- * Return: Always 0.
  */
 
 void puts2(char *str)
 {
-	int i;
-
-	i = 0
-
 	for ( ; *str != '\0'; str += 2)
 	{
-		i++;
 		_putchar(*str);
 	}
 	_putchar('\n');
