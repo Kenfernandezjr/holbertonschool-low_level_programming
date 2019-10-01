e726e5cf34ffb8fbf175d7f18d6c8db9a39e64e4
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-01 17:41:25 +0000

    printing a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+
+/**
+ * print_rev - Function to start this project.
+ * @s: str is the phrase that needed to be printed backwards.
+ * Return: Void/Null.
+ */
+
+void print_rev(char *s)
+{
+	int c;
+
+	c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	s--;
+	for (; c >= 0; c--, s--)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
