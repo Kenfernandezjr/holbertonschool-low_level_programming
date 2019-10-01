d9ee24a5440e1fb0d988df1ab53ed0e692cbc549
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-01 00:52:18 +0000

    counting the length of holberton

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - function to start this project with.
+ *
+ * @s: is the interger that stores the characters holberton.
+ *
+ * Return: - Void/Null.
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	c = 0;
+
+	for (;*s !=0; s++)
+	{
+		c++;
+	}
+	return c;
+}
