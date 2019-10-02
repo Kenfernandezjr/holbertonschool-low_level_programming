7ef8da801e0618bea64aa2e29fbc712edffe25a3
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-02 04:03:44 +0000

    prints half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Function for this project.
+ *
+ * @str: it holds all place holders the string.
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			j = i / 2;
+		}
+
+		else
+		{
+		j = (i + 1) / 2;
+		}
+	}
+	for ( ; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
