106cbf8e88394f77cb4e40c8d8ff666ac4fff7a9
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-08 03:21:02 +0000

    life has new adventures

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - function for this project.
+ * @s: is the str1.
+ * @c: is the str2.
+ * Return: - Null.
+ */
+char *_strchr(char *s, char c)
+{
+	int counter;
+
+	for (counter = 0; s[counter]; counter++)
+	{
+		if (s[counter] == c)
+		{
+			return (s + counter);
+		}
+	}
+	if (s[counter])
+	{
+		return (s + counter);
+	}
+	return (NULL);
+}
