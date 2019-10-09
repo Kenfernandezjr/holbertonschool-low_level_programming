c7106723a9ce792de345e858f48cc6f32ed7dec9
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-09 16:30:25 +0000

    Print string using recurison

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function to use recursion.
+ *
+ * @s: - holds the string Betty Holberton.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		puts(s);
+	}
+}
