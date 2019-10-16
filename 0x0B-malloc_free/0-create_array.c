b5032fe18ab730056c63a0c9f0ad91463befa3c2
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-16 03:21:07 +0000

    First Malloc project

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include  "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - function
+ *
+ * @size: parameter of the work
+ *
+ * @c: char for using to count the address
+ *
+ * Return: - the pointer ar
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ar;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	ar = malloc(size * sizeof(ar));
+	if (ar == '\0')
+	{
+		return ('\0');
+	}
+	for (i = 0; i < size; i++)
+		ar[i] = c;
+
+	return (ar);
+}
