7591fde2435448a0e3dab388c095a07c34fa252b
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-23 00:20:29 +0000

    header with structure dog type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure dog.
+ * @name:- char.
+ * @age:- float.
+ * @owner:- owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
