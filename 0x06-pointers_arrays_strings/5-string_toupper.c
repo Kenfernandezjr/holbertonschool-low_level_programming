13ad0536a337792bfff3d603795b1e34e4e3dbbc
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-29 21:09:03 +0000

    String converts lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,7 +9,7 @@ char *string_toupper(char *c)
 	int counter;
 
 	counter = 0;
-	for ( ;c[counter] != '\0'; counter++)
+	for ( ; c[counter] != '\0'; counter++)
 	{
 		if (c[counter] >= 'a' && c[counter] <= 'z')
 		{
