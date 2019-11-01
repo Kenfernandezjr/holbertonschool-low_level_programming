1e1b470c230302daa6a567b4d7c15d2e2d5c7971
Kenfernandezjr <Kenfernandezjr@gmail.com>
2019-11-01 04:07:04 +0000

    Write a function that prints

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * tortoise - function for printing this string first.
+ */
+
+void tortoise(void)
+{
+
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my
+back!\n");
+
+}
