0b5b2672fcbeb13448775a2dcfbc7b85d8926f76
Kenfernandezjr <Kenfernandezjr@gmail.com>
2019-11-20 16:07:37 +0000

    printing using standard error & write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints the string to a standard error output.
+ * Return:- 1 as project asked.
+ */
+
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      60);
+	return (1);
+}
