a2258919a7eb87697c4c8731ddeb4b3a25452d9b
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-19 04:23:03 +0000

    Create a Hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,34 +4,34 @@
 #include <string.h>
 #include <stdio.h>
 
-/*
+/**
+ * hash_table_create:- main function to create a table.
+ * @size:- the newhash size.
+ * Return:- new  hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *newHash;
-  unsigned long int counter = 0;
+	hash_table_t *newHash;
+	unsigned long int counter = 0;
 
-  newHash = malloc(sizeof(hash_table_t));
+	newHash = malloc(sizeof(hash_table_t));
 
-  if (newHash == NULL)
-  {
-    return (NULL);
-  }
-  newHash->array = malloc(sizeof(hash_table_t) * size);
-  if (newHash->array == NULL)
-    {
-      free(newHash);
-      return (NULL);
-    }
-  
-  while(counter > size)
-  {
-    newHash->array[counter] = NULL;
-    counter++;
-  }
-  newHash->size = size;
+	if (newHash == NULL)
+	{
+		return (NULL);
+	}
+	newHash->array = malloc(sizeof(hash_table_t) * size);
+	if (newHash->array == NULL)
+	{
+		free(newHash);
+		return (NULL);
+	}
 
-  return (newHash);
+	while (counter > size)
+	{
+		newHash->array[counter] = NULL;
+		counter++;
+	}
+	newHash->size = size;
+	return (newHash);
 }
-
-  
