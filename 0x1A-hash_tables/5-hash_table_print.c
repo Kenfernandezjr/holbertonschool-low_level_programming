16e9edaa04a66fdc57c4c393cf9e1b59b774faaf
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-20 06:48:42 +0000

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print- main function to print hash table
+ * @ht:-
+ * Return:-
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+}
