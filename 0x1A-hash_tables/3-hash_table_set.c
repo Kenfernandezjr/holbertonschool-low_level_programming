8c2113771b2554ecbcbf0b0ddb211983878a0ee5
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-27 04:42:47 +0000

    fixed the file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,30 +13,61 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newNode;
-	int index;
 
-	if (ht == NULL)
+	if (ht == NULL || strcmp(key, "") == 0)
 	{
 		return (0);
 	}
-	if (key == NULL)
-	{
-		return (0);
-	}
-
-	index = (key_index((const char *)key) ht->size);
-
-	newNode = ht->array[index];
-
-	while (newNode != NULL)
-	{
-		newNode = newNode->next;
-	}
-
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 	{
 		return (0);
 	}
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	newNode->next = NULL;
+	insert_node(newNode, ht);
 
+	return (1);
+
+}
+
+/**
+ * insert_node - main function for inserting a newNode.
+ * @node: - node to insert.
+ * @htable: - hash table and structure.
+ */
+void insert_node(hash_node_t *node, hash_table_t *htable)
+{
+	unsigned long int index = key_index((unsigned char *)node->key, htable->size);
+	hash_node_t *temp = htable->array[index];
+
+	if (htable->array[index] != NULL)
+	{
+		while (temp != NULL)
+		{
+			if (strcmp(temp->key, node->key) == 0)
+			{
+				break;
+			}
+			temp = temp->next;
+		}
+		if (temp == NULL)
+		{
+			node->next = htable->array[index];
+			htable->array[index] = node;
+		}
+		else
+		{
+			free(temp->value);
+			temp->value = strdup(node->value);
+			free(node->key);
+			free(node->value);
+			free(node);
+		}
+	}
+	else
+	{
+		htable->array[index] = node;
+	}
 }
