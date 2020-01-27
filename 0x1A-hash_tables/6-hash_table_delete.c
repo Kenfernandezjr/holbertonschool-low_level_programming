910b639cfdebbfea2b922635b87210a21720a5bc
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-27 07:14:47 +0000

    test deleting hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - main function to delete hash table
+ * @ht:- hash table structure
+ *
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int index;
+	hash_node_t *temp;
+	hash_node_t *headNode;
+
+	while (index < ht->size)
+	{
+
+		headNode = ht->array[index];
+		while (headNode != NULL)
+		{
+			temp = headNode->next;
+			free(headNode->key);
+			free(headNode->value);
+			free(headNode);
+			headNode = temp;
+		}
+		index++;
+	}
+	free(ht);
+
+}
