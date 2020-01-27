836ea072a3d8779282346b2f0e3d60e5d331d499
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-27 07:40:10 +0000

    testing print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,4 +11,24 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *headNode;
+	unsigned long int index = 0;
+
+	if (ht == NULL)
+	{
+
+	}
+	headNode = ht->array[index];
+	while (index < ht->size)
+	{
+
+		while (headNode != NULL)
+		{
+
+			printf("'%s': '%s'", headNode->key, headNode->value);
+			headNode = headNode->next;
+		}
+		index++;
+	}
+	printf("{%s}\n", ht->array[index]);
 }
