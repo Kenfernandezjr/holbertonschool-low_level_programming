e36d7f66b68b5e4fb58c01e6e10f90dff544d271
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-01-27 04:45:52 +0000

    added custom insert

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,6 +37,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-int _strcmp(char *s1, char *s2);
+void insert_node(hash_node_t *node, hash_table_t *htable);
 
 #endif /* HASH_TABLES_H */
